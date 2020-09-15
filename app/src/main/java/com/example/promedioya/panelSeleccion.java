2e5045b178e8fa94a3a726e157a195fe9520297a
PPinnola <p.pinnola28@gmail.com>
2020-09-15 19:09:44 -0300

    creacion de dos mockups

---
+++ app/src/main/java/com/example/promedioya/panelSeleccion.java
@@ -0,0 +1,14 @@
+package com.example.promedioya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class panelSeleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_seleccion);
+    }
+}
