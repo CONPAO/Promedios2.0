ab8f455e62133043d19227153ad421f7d0d7947c
Mitzyf <mitzyflores.fc@gmail.com>
2020-09-17 19:44:47 -0300

    PAOPAO

--- app/src/main/java/com/example/promedioya/MainActivity.java
+++ app/src/main/java/com/example/promedioya/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.promedioya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button aCantidadNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aCantidadNotas= (Button) findViewById(R.id.calcularProm);
+        aCantidadNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent aCantidadNotas = new Intent(MainActivity.this, cantidad_notas.class);
+                startActivity (aCantidadNotas);
+            }
+        });
+    }
+
+    public class Cantidad_de_semestres extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/promedioya/cantidad_notas.java
@@ -0,0 +1,14 @@
+package com.example.promedioya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cantidad_notas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
