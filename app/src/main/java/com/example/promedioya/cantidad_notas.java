15831e37ace00515667bfe1026fdb943b5ad4d1a
PPinnola <p.pinnola28@gmail.com>
2020-09-17 23:48:03 -0300

    intento de arreglar cosas :c

--- app/src/main/java/com/example/promedioya/cantidad_notas.java
+++ app/src/main/java/com/example/promedioya/cantidad_notas.java
@@ -9,6 +9,6 @@ public class cantidad_notas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.frmcantidad_notas);
     }
 }
